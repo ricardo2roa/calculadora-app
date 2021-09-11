99e1d1914ce8622365305f229c0f2eae889764a8
Ricardo Roa <ricardo2roa@gmail.com>
2021-09-10 23:38:14 -0500

    Calculadora
    Creación de calculadora básica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora-app';
+  sum1:number=0
+  sum2:number=0
+  suma:number = 0
+  sumar(){
+    this.suma = this.sum1 + this.sum2
+  }
 }
